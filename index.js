aeac0758b43414146fcc28ea0e188baa7b8c0e83
xinshangshangxin <xinshangshangxin@gmail.com>
2017-04-23 14:28:28 +0800

    refactor: entry change

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 
-// init global required
-global._ = require('lodash');
-global.Deferred = require('jquery-deferred').Deferred;
-global.whenCallSequentially = require('./lib/whenCallSequentially');
+module.exports = require('./lib/bundle');
+
